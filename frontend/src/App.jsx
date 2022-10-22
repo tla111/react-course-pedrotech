bc5089ca5a9b34422e0f550f32d05eefe77f2f64
Tim La <tlamarketing111@gmail.com>
2022-10-21 20:17:35 -0400

    created Job component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ const App = () => {
     <div className="App">
       <User name="Pedro" age={21} email="PedroTech@gmail.com" />
       <User name="Tim" age={29} email="TimLa@gmail.com" />
+      <Job />
     </div>
   )
 }
@@ -17,6 +18,14 @@ const User = ({ name, age, email }) => {
       <h1>{name}</h1>
       <h1>{age}</h1>
       <h1>{email}</h1>
+    </div>
+  )
+}
+
+const Job = () => {
+  return (
+    <div>
+      Job
     </div>
   )
 }
