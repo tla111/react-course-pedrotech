0352da2dec3c57df8d83a944739a8279312d6fb3
Tim La <tlamarketing111@gmail.com>
2022-10-21 20:23:42 -0400

    able to render job component properly in user component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,20 +5,22 @@ const App = () => {
 
   return (
     <div className="App">
-      <User name="Pedro" age={21} email="PedroTech@gmail.com" />
-      <User name="Tim" age={29} email="TimLa@gmail.com" />
-      <Job salary={90000} position="Senior SDE" company="Amazon" />
-      <Job salary={12000} position="Junior SDE" company="Google" />
+      <User name="Pedro" age={21} email="PedroTech@gmail.com" salary={90000} position="Senior SDE" company="Amazon" />
+      <User name="Tim" age={29} email="TimLa@gmail.com" salary={12000} position="Junior SDE" company="Google" />
+      {/* <Job salary={90000} position="Senior SDE" company="Amazon" /> */}
+      {/* <Job salary={12000} position="Junior SDE" company="Google" /> */}
     </div>
   )
 }
 
-const User = ({ name, age, email }) => {
+const User = ({ name, age, email, salary, position, company }) => {
   return (
     <div>
       <h1>{name}</h1>
       <h1>{age}</h1>
       <h1>{email}</h1>
+      <Job salary={salary} position={position} company={company} />
+      <hr />
     </div>
   )
 }
