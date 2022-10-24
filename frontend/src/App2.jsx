17e4cbd5da67e5207ee147318531c664a74fc662
Tim La <tlamarketing111@gmail.com>
2022-10-24 14:21:18 -0400

    used boolean with tenerary operator

--- frontend/src/App2.jsx
+++ frontend/src/App2.jsx
@@ -3,11 +3,12 @@ import './App.css'
 
 const App2 = () => {
     const age = 17;
+    const isTrue = false;
 
     return (
         <div className="App">
             {age >= 18 ? <h1>Over Age</h1> : <h2>Under Age</h2>}
-            <h1 style={{ color: "red" }}>Color</h1>
+            <h1 style={{ color: isTrue ? "red" : "blue" }}>Color</h1>
         </div>
     )
 }
