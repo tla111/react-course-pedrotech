7dacf10560ade945d899d105e214bcf37e91807c
Tim La <tlamarketing111@gmail.com>
2022-10-24 14:24:16 -0400

    created third app component

---
+++ frontend/src/App3.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './App.css'
+
+
+const App3 = () => {
+    return (
+        <div>App3</div>
+    )
+}
+
+export default App3
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import App from './App'
-import App2 from './App2'
+// import App2 from './App2'
+import App3 from './App3'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App2 />
+    <App3 />
   </React.StrictMode>
 )
