a363aa4b5439e373cb7e3e236448664019dc6d93
Tim La <tlamarketing111@gmail.com>
2022-10-24 14:19:42 -0400

    used inline style to style color

--- frontend/src/App2.jsx
+++ frontend/src/App2.jsx
@@ -7,6 +7,7 @@ const App2 = () => {
     return (
         <div className="App">
             {age >= 18 ? <h1>Over Age</h1> : <h2>Under Age</h2>}
+            <h1 style={{ color: "red" }}>Color</h1>
         </div>
     )
 }
