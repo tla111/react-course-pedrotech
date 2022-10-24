7cab56548abfe1e1fdec7b823aac97760738b4d4
Tim La <tlamarketing111@gmail.com>
2022-10-24 14:18:26 -0400

    used ternary operator to reduce code

--- frontend/src/App2.jsx
+++ frontend/src/App2.jsx
@@ -3,17 +3,10 @@ import './App.css'
 
 const App2 = () => {
     const age = 17;
-    let sentence;
-
-    if (age >= 18) {
-        sentence = <h1>Over Age</h1>
-    } else {
-        sentence = <h2>Under Age</h2>
-    }
 
     return (
         <div className="App">
-            {sentence}
+            {age >= 18 ? <h1>Over Age</h1> : <h2>Under Age</h2>}
         </div>
     )
 }
