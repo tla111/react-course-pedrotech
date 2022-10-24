e6fca4ed4029b5924f27bebc4b3c8aea1303bc02
Tim La <tlamarketing111@gmail.com>
2022-10-24 14:16:11 -0400

    using conditional rendering to display sentence

--- frontend/src/App2.jsx
+++ frontend/src/App2.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 import './App.css'
 
 const App2 = () => {
+    const age = 17;
+    let sentence;
+
+    if (age >= 18) {
+        sentence = <h1>Over Age</h1>
+    } else {
+        sentence = <h2>Under Age</h2>
+    }
+
     return (
         <div className="App">
-
+            {sentence}
         </div>
     )
 }
