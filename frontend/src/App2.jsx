5ca3bbbe718220f4457a207d78a8efcf3ea68e3b
Tim La <tlamarketing111@gmail.com>
2022-10-24 14:22:58 -0400

    used && to display button or not

--- frontend/src/App2.jsx
+++ frontend/src/App2.jsx
@@ -9,6 +9,8 @@ const App2 = () => {
         <div className="App">
             {age >= 18 ? <h1>Over Age</h1> : <h2>Under Age</h2>}
             <h1 style={{ color: isTrue ? "red" : "blue" }}>Color</h1>
+            {isTrue && <button>Button</button>}
+            {!isTrue && <button>Button</button>}
         </div>
     )
 }
