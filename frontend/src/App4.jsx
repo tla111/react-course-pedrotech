c8c9f303bc3f09c01af9169ccee1771be024bf7b
Tim La <tlamarketing111@gmail.com>
2022-10-25 11:17:28 -0400

    created age state & button

--- frontend/src/App4.jsx
+++ frontend/src/App4.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 import './App.css'
 
 const App4 = () => {
+    const age = 0;
     return (
-        <div className="App">App4</div>
+        <div className="App">
+            {age}
+            <button>Increase Age</button>
+        </div>
     )
 }
 
