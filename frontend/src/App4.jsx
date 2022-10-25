7bdb062a0de02e338ebe150c9328b2253424b93b
Tim La <tlamarketing111@gmail.com>
2022-10-25 11:24:25 -0400

    created function for onChange event

--- frontend/src/App4.jsx
+++ frontend/src/App4.jsx
@@ -2,11 +2,15 @@ import React, { useState } from 'react'
 import './App.css'
 
 const App4 = () => {
-    const [inputValue, setInputValue] = useState("")
+    const [inputValue, setInputValue] = useState("");
+
+    const handleInputChange = (event) => {
+        console.log(event.target.value);
+    }
 
     return (
         <div className="App">
-            <input type="text" />
+            <input type="text" onChange={handleInputChange} />
             {inputValue}
         </div>
     )
