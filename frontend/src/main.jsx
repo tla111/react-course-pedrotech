479cd12807aaa1438b0599c8e5c741222681d744
Tim La <tlamarketing111@gmail.com>
2022-10-25 11:33:47 -0400

    created 5th app

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client'
 // import App from './App'
 // import App2 from './App2'
 // import App3 from './App3'
-import App4 from './App4'
+// import App4 from './App4'
+import App5 from './App5'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App4 />
+    <App5 />
   </React.StrictMode>
 )
 
