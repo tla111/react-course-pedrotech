af8be5e971aa9263c61601c6c7451d91de6923b9
Tim La <tlamarketing111@gmail.com>
2022-10-25 11:29:42 -0400

    used state to change color

--- frontend/src/App4.jsx
+++ frontend/src/App4.jsx
@@ -2,12 +2,12 @@ import React, { useState } from 'react'
 import './App.css'
 
 const App4 = () => {
-    const [showText, setShowText] = useState(false);
+    const [textColor, setTextColor] = useState("red")
 
     return (
         <div className="App">
-            <button onClick={() => setShowText(!showText)}>Show/Hide</button>
-            {showText && <h1>HI MY NAME IS PEDRO</h1>}
+            <button onClick={() => setTextColor(textColor === "black" ? "red" : "black")}>Show/Hide</button>
+            <h1 style={{ color: textColor }}>HI MY NAME IS PEDRO</h1>
         </div>
     )
 }
