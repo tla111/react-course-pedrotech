9771e7b9fc5013096030f75f9141a79a6516f5e9
Tim La <tlamarketing111@gmail.com>
2022-10-26 08:48:42 -0400

    created todoList state

--- frontend/src/App5.jsx
+++ frontend/src/App5.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
 
 const App5 = () => {
+    const [todoList, setTodoList] = useState([])
+
     return (
         <div className="App">
             <div className="addTask">
-
+                <input />
+                <button>Add Task</button>
             </div>
             <div className="list">
 
