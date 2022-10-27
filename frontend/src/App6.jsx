5a6a538b50a1cad9466082df86b697f88114fcd1
Tim La <tlamarketing111@gmail.com>
2022-10-27 09:15:11 -0400

    created 6th app

---
+++ frontend/src/App6.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './App.css'
+
+const App6 = () => {
+    return (
+        <div className="App">App6</div>
+    )
+}
+
+export default App6
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,11 +4,12 @@ import ReactDOM from 'react-dom/client'
 // import App2 from './App2'
 // import App3 from './App3'
 // import App4 from './App4'
-import App5 from './App5'
+// import App5 from './App5'
+import App6 from './App6'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App5 />
+    <App6 />
   </React.StrictMode>
 )
 
