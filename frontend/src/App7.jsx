289f6f9cf5e3df1196d2d940898ea5d4d31617a0
Tim La <tlamarketing111@gmail.com>
2022-10-28 10:12:10 -0400

    able to fetch api data

--- frontend/src/App7.jsx
+++ frontend/src/App7.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import './App.css'
 
+fetch("https://catfact.ninja/fact")
+    .then((res) => res.json())
+    .then((data) => {
+        console.log(data);
+    })
+
 const App7 = () => {
     return (
         <div className="App">App7</div>
