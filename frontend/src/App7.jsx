1667e3b80f782549a63a9cf036dfca51cfc76863
Tim La <tlamarketing111@gmail.com>
2022-10-28 10:15:53 -0400

    stored catFact

--- frontend/src/App7.jsx
+++ frontend/src/App7.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import Axios from 'axios'
 
 const App7 = () => {
+    const [catFact, setCatFact] = useState("")
+
     Axios.get("https://catfact.ninja/fact").then((res) => {
-        console.log(res.data);
+        setCatFact(res.data.fact);
     })
 
     return (
-        <div className="App">App7</div>
+        <div className="App">
+            <button>Generate Cat Fact</button>
+            <p>{catFact}</p>
+        </div>
     )
 }
 
