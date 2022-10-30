7cfa3650fc471bdfe71f96edd16bc2040c54c2d3
Tim La <tlamarketing111@gmail.com>
2022-10-30 07:16:00 -0400

    created profile component

--- frontend/src/App9.jsx
+++ frontend/src/App9.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import Home from "./Pages/Home"
-import Menu from './Pages/Menu'
+import Profile from './Pages/Profile'
 import Contact from './Pages/Contact'
 
 const App9 = () => {
@@ -11,12 +11,12 @@ const App9 = () => {
             <Router>
                 <div>
                     <Link to="/">Home</Link>
-                    <Link to="/menu">Menu</Link>
+                    <Link to="/profile">Profile</Link>
                     <Link to="/contact">Contact</Link>
                 </div>
                 <Routes>
                     <Route path="/" element={<Home />} />
-                    <Route path="/menu" element={<Menu />} />
+                    <Route path="/profile" element={<Profile />} />
                     <Route path="/contact" element={<Contact />} />
                     <Route path="*" element={<h1>PAGE NOT FOUND</h1>} />
                 </Routes>
