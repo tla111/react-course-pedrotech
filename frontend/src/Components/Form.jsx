41aadd9c89267e22bdae0f4b398ec20b8811ddc9
Tim La <tlamarketing111@gmail.com>
2022-10-31 13:23:00 -0400

    imported all from yup

--- frontend/src/Components/Form.jsx
+++ frontend/src/Components/Form.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useForm } from 'react-hook-form'
+import * as yup from 'yup'
 
 const Form = () => {
     const { register, handleSubmit } = useForm()
@@ -11,7 +12,7 @@ const Form = () => {
         <form style={{ display: "flex", flexDirection: "column" }} onSubmit={handleSubmit(onSubmit)}>
             <input type="text" placeholder="Full Name..." {...register("fullName")} />
             <input type="text" placeholder="Email..." {...register("email")} />
-            <input type="text" placeholder="Age..."{...register("age")} />
+            <input type="number" placeholder="Age..."{...register("age")} />
             <input type="password" placeholder="Password" {...register("password")} />
             <input type="password" placeholder="Confirm Password..." {...register("confirmPassword")} />
             <input type="submit" />
