67fddd20271f41ccec9679b54ebd98b1c2fa160c
Tim La <tlamarketing111@gmail.com>
2022-10-31 10:38:37 -0400

    added boiler code

--- frontend/src/App10.jsx
+++ frontend/src/App10.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import './App.css'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Home from "./Pages/Home"
+import Menu from './Pages/Menu'
+import Contact from './Pages/Contact'
 
 const App10 = () => {
     return (
-        <div>App10</div>
+        <div className="App">
+            <Router>
+                <div>
+                    <Link to="/">Home</Link>
+                    <Link to="/menu">Menu</Link>
+                    <Link to="/contact">Contact</Link>
+                </div>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/menu" element={<Menu />} />
+                    <Route path="/contact" element={<Contact />} />
+                    <Route path="*" element={<h1>PAGE NOT FOUND</h1>} />
+                </Routes>
+            </Router>
+        </div>
     )
 }
 
