f06c30cfc6147948874d1b4acf6030d472d17b32
Tim La <tlamarketing111@gmail.com>
2022-10-31 10:48:05 -0400

    using isLoading

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -17,9 +17,14 @@ import { useQuery } from '@tanstack/react-query'
 import Axios from "axios"
 
 const Home = () => {
-    const { data } = useQuery(["cat"], () => {
+    const { data, isLoading } = useQuery(["cat"], () => {
         return Axios.get("https://catfact.ninja/fact").then((res) => res.data)
     })
+
+    if (isLoading) {
+        return <h1>Loading...</h1>
+    }
+
     return (
         <div>
             <h1>THIS IS THE HOMEPAGE</h1>
