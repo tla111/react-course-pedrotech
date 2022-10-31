5252376338fe21ca6d34b6b0bbd677188fb80bcf
Tim La <tlamarketing111@gmail.com>
2022-10-31 13:21:38 -0400

    able to submit data

--- frontend/src/Components/Form.jsx
+++ frontend/src/Components/Form.jsx
@@ -4,16 +4,16 @@ import { useForm } from 'react-hook-form'
 const Form = () => {
     const { register, handleSubmit } = useForm()
 
-    const onSubmit = () => {
-        console.log("Hello World");
+    const onSubmit = (data) => {
+        console.log(data);
     }
     return (
         <form style={{ display: "flex", flexDirection: "column" }} onSubmit={handleSubmit(onSubmit)}>
-            <input type="text" placeholder="Full Name..." />
-            <input type="text" placeholder="Email..." />
-            <input type="text" placeholder="Age..." />
-            <input type="password" placeholder="Password" />
-            <input type="password" placeholder="Confirm Password..." />
+            <input type="text" placeholder="Full Name..." {...register("fullName")} />
+            <input type="text" placeholder="Email..." {...register("email")} />
+            <input type="text" placeholder="Age..."{...register("age")} />
+            <input type="password" placeholder="Password" {...register("password")} />
+            <input type="password" placeholder="Confirm Password..." {...register("confirmPassword")} />
             <input type="submit" />
         </form>
     )
