634b86463331140666d11fd0e21c66b0ae122a8e
Tim La <tlamarketing111@gmail.com>
2022-10-31 10:37:58 -0400

    created 10th app

---
+++ frontend/src/App10.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App10 = () => {
+    return (
+        <div>App10</div>
+    )
+}
+
+export default App10
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,11 +8,12 @@ import ReactDOM from 'react-dom/client'
 // import App6 from './App6'
 // import App7 from './App7'
 // import App8 from './App8'
-import App9 from './App9'
+// import App9 from './App9'
+import App10 from './App10'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App9 />
+    <App10 />
   </React.StrictMode>
 )
 
