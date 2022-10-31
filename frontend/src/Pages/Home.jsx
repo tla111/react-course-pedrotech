7828eade30c637a2ed13dcabb14a7d8e20279ae0
Tim La <tlamarketing111@gmail.com>
2022-10-31 10:49:28 -0400

    using isError

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -17,9 +17,13 @@ import { useQuery } from '@tanstack/react-query'
 import Axios from "axios"
 
 const Home = () => {
-    const { data, isLoading } = useQuery(["cat"], () => {
+    const { data, isLoading, isError } = useQuery(["cat"], () => {
         return Axios.get("https://catfact.ninja/fact").then((res) => res.data)
     })
+
+    if (isError) {
+        return <h1>Error</h1>
+    }
 
     if (isLoading) {
         return <h1>Loading...</h1>
