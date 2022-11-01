4193fcf35762145792e4dcf906979bbc3ba19476
Tim La <tlamarketing111@gmail.com>
2022-11-01 10:19:05 -0400

    saving work

--- frontend/src/App12.jsx
+++ frontend/src/App12.jsx
@@ -1,13 +1,14 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './App.css'
+import { useToggle } from './Components/useToggle'
 
 
 const App12 = () => {
-    const [isVisible, setIsVisible] = useState(false)
+    const [isVisible, toggle] = useToggle()
 
     return (
         <div className="App">
-            <button onClick={() => setIsVisible((prev) => !prev)}>
+            <button onClick={toggle}>
                 {isVisible ? "Hide" : "Show"}
             </button>
             {isVisible && <h1>Hidden Text</h1>}
